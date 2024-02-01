NilFoundation/crypto3-zk

Added top bit field grinding variant.

include/nil/crypto3/zk/snark/systems/plonk/placeholder/gates_argument.hpp
@@ -30,11 +30,13 @@
 
 #include <unordered_map>
 #include <iostream>
+#include <memory>
 
 #include <nil/crypto3/math/polynomial/polynomial.hpp>
 #include <nil/crypto3/math/polynomial/shift.hpp>
 #include <nil/crypto3/math/domains/evaluation_domain.hpp>
 #include <nil/crypto3/math/algorithms/make_evaluation_domain.hpp>
+#include <nil/crypto3/zk/snark/arithmetization/plonk/assignment.hpp>
 
 #include <nil/crypto3/hash/sha2.hpp>
 

NilFoundation/actor-zk

include/nil/actor/zk/snark/systems/plonk/placeholder/gates_argument.hpp
@@ -30,11 +30,13 @@
 
 #include <unordered_map>
 #include <iostream>
+#include <memory>
 
 #include <nil/actor/math/polynomial/polynomial.hpp>
 #include <nil/actor/math/polynomial/shift.hpp>
 #include <nil/actor/math/domains/evaluation_domain.hpp>
 #include <nil/actor/math/algorithms/make_evaluation_domain.hpp>
+#include <nil/actor/zk/snark/arithmetization/plonk/assignment.hpp>
 
 #include <nil/crypto3/hash/sha2.hpp>
 