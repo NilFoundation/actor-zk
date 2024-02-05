d90f280cae555de8f2b0a884f800ba42a56ea160
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2024-02-05 16:37:30 +0200

    Added common interface for poseidon and other hashesh #34

--- include/nil/actor/zk/commitments/detail/polynomial/proof_of_work.hpp
+++ include/nil/actor/zk/commitments/detail/polynomial/proof_of_work.hpp
@@ -39,72 +39,23 @@ namespace nil {
     namespace actor {
         namespace zk {
             namespace commitments {
-                template<typename TranscriptHashType, typename OutType = std::uint32_t, std::uint32_t MASK=0xFFFF0000>
-                class proof_of_work {
+
+                template<typename TranscriptHashType,
+                    typename output_type,
+                    std::uint8_t grinding_bits,
+                    typename Enable = void>
+                class proof_of_work;
+
+                template<typename TranscriptHashType,
+                    typename output_type,
+                    std::uint8_t grinding_bits>
+                class proof_of_work<TranscriptHashType, output_type, grinding_bits, typename std::enable_if_t<!crypto3::hashes::is_poseidon<TranscriptHashType>::value>> {
                 public:
-                    using transcript_hash_type = TranscriptHashType;
-                    using transcript_type = transcript::fiat_shamir_heuristic_sequential<transcript_hash_type>;
-                    using output_type = OutType;
-
-                    constexpr static std::uint32_t mask = MASK;
-
-                    static inline boost::property_tree::ptree get_params() {
-                        boost::property_tree::ptree params;
-                        params.put("mask", mask);
-                        return params;
-                    }
-
-                    static inline OutType generate(transcript_type &transcript) {
-                        output_type proof_of_work = std::rand();
-                        output_type result;
-                        std::vector<std::uint8_t> bytes(4);
-
-                        while( true ) {
-                            transcript_type tmp_transcript = transcript;
-                            bytes[0] = std::uint8_t((proof_of_work&0xFF000000)>>24);
-                            bytes[1] = std::uint8_t((proof_of_work&0x00FF0000)>>16);
-                            bytes[2] = std::uint8_t((proof_of_work&0x0000FF00)>>8);
-                            bytes[3] = std::uint8_t(proof_of_work&0x000000FF);
-
-                            tmp_transcript(bytes);
-                            result = tmp_transcript.template int_challenge<output_type>();
-                            if ((result & mask) == 0)
-                                break;
-                            proof_of_work++;
-                        }
-                        transcript(bytes);
-                        result = transcript.template int_challenge<output_type>();
-                        return proof_of_work;
-                    }
-
-                    static inline bool verify(transcript_type &transcript, output_type proof_of_work) {
-                        std::vector<std::uint8_t> bytes(4);
-                        bytes[0] = std::uint8_t((proof_of_work&0xFF000000)>>24);
-                        bytes[1] = std::uint8_t((proof_of_work&0x00FF0000)>>16);
-                        bytes[2] = std::uint8_t((proof_of_work&0x0000FF00)>>8);
-                        bytes[3] = std::uint8_t(proof_of_work&0x000000FF);
-                        transcript(bytes);
-                        output_type result = transcript.template int_challenge<output_type>();
-                        return ((result & mask) == 0);
-                    }
-                };
-
-                // Note that the interface here is slightly different from the one above:
-                // amount of bits for grinding instead of the mask.
-                // This was done because the actual mask is applied to the high bits instead of the low bits
-                // which makes manually setting the mask error-prone.
-                template<typename TranscriptHashType, typename FieldType, std::uint8_t GrindingBits=16>
-                class field_proof_of_work {
-                public:
-                    using transcript_hash_type = TranscriptHashType;
-                    using transcript_type = transcript::fiat_shamir_heuristic_sequential<transcript_hash_type>;
-                    using value_type = typename FieldType::value_type;
-                    using integral_type = typename FieldType::integral_type;
-
-                    constexpr static const integral_type mask =
-                        (GrindingBits > 0 ?
-                            ((integral_type(2) << GrindingBits - 1) - 1) << (FieldType::modulus_bits - GrindingBits)
+                    constexpr static output_type mask = (grinding_bits > 0 ?
+                            ((output_type(2) << grinding_bits ) - 1) << (sizeof(output_type)*8 - grinding_bits)
                             : 0);
+                    using transcript_hash_type = TranscriptHashType;
+                    using transcript_type = transcript::fiat_shamir_heuristic_sequential<transcript_hash_type>;
 
                     static inline boost::property_tree::ptree get_params() {
                         boost::property_tree::ptree params;
@@ -112,11 +63,9 @@ namespace nil {
                         return params;
                     }
 
-                    static inline value_type generate(transcript_type &transcript,
-                        nil::crypto3::random::algebraic_engine<FieldType> random_engine) {
+                    static inline output_type generate(transcript_type &transcript) {
 
-                        value_type pow_seed = random_engine();
-
+                        output_type pow_seed = 0;
                         /* Enough work for ~ two minutes on 48 cores */
                         std::size_t per_block = 1<<23;
 
@@ -126,15 +75,23 @@ namespace nil {
                         while( true ) {
                             math::detail::block_execution(
                                 per_block, smp::count,
-                                [&transcript, &pow_seed, &challenge_found, &pow_value_offset](std::size_t pow_start, std::size_t pow_finish) {
+                                [&transcript, &pow_seed, &challenge_found, &pow_value_offset]
+                                (std::size_t pow_start, std::size_t pow_finish) {
                                     std::size_t i = pow_start;
                                     while ( i < pow_finish ) {
                                         if (challenge_found) {
                                             break;
                                         }
+                                        std::vector<std::uint8_t> bytes(sizeof(output_type));
+                                        for(int j = 0; j < sizeof(output_type) ; ++j ) {
+                                            bytes[j] = std::uint8_t( ((pow_seed+i) >> (sizeof(output_type)-1-j)*8) & 0xFF);
+                                        }
+
                                         transcript_type tmp_transcript = transcript;
-                                        tmp_transcript(pow_seed + i);
-                                        integral_type pow_result = integral_type(tmp_transcript.template challenge<FieldType>().data);
+
+                                        output_type proof_of_work_value = pow_seed + i;
+                                        tmp_transcript(bytes);
+                                        output_type pow_result = tmp_transcript.template int_challenge<output_type>();
                                         if ( ((pow_result & mask) == 0) && !challenge_found ) {
                                             challenge_found = true;
                                             pow_value_offset = i;
@@ -149,15 +106,100 @@ namespace nil {
                             }
                             pow_seed += per_block;
                         }
+                        output_type proof_of_work_value = pow_seed + pow_value_offset;
 
-                        transcript(pow_seed + (std::size_t)pow_value_offset);
-                        transcript.template challenge<FieldType>();
-                        return pow_seed + (std::size_t)pow_value_offset;
+                        std::vector<std::uint8_t> bytes(sizeof(output_type));
+                        for(int j = 0; j < sizeof(output_type) ; ++j ) {
+                            bytes[j] = std::uint8_t( (proof_of_work_value >> (sizeof(output_type)-1-j)*8) & 0xFF);
+                        }
+                        transcript(bytes);
+                        auto pow_result = transcript.template int_challenge<output_type>();
+                        output_type result = 0;
+                        for (int i = 0; i < sizeof(output_type); ++i ) {
+                            result <<= 8;
+                            result |= bytes[i];
+                        }
+                        return result;
                     }
 
-                    static inline bool verify(transcript_type &transcript, value_type const& proof_of_work) {
+                    static inline bool verify(transcript_type &transcript, output_type proof_of_work) {
+                        std::vector<std::uint8_t> bytes(sizeof(output_type));
+                        for(int j = 0; j < sizeof(output_type) ; ++j ) {
+                            bytes[j] = std::uint8_t( (proof_of_work>> (sizeof(output_type)-1-j)*8) & 0xFF);
+                        }
+                        transcript(bytes);
+                        output_type result = transcript.template int_challenge<output_type>();
+                        return ((result & mask) == 0);
+                    }
+                };
+
+                /* Specialization for poseidon */
+                template<typename TranscriptHashType,
+                    std::uint8_t grinding_bits>
+                class proof_of_work<
+                    TranscriptHashType,
+                    typename TranscriptHashType::policy_type::field_type::value_type,
+                    grinding_bits,
+                    typename std::enable_if_t<crypto3::hashes::is_poseidon<TranscriptHashType>::value> > {
+                public:
+                    using transcript_hash_type = TranscriptHashType;
+                    using transcript_type = transcript::fiat_shamir_heuristic_sequential<transcript_hash_type>;
+                    using field_type = typename transcript_hash_type::policy_type::field_type;
+                    using output_type = typename field_type::value_type;
+                    using integral_type = typename output_type::integral_type;
+                    constexpr static integral_type mask = (grinding_bits > 0 ?
+                            ((integral_type(2) << (grinding_bits-1) ) - 1) << (field_type::modulus_bits - grinding_bits)
+                            : 0);
+
+                    static inline boost::property_tree::ptree get_params() {
+                        boost::property_tree::ptree params;
+                        params.put("mask", mask);
+                        return params;
+                    }
+
+                    static inline output_type generate(transcript_type &transcript) {
+                        output_type pow_seed = 0;
+                        /* Enough work for ~ two minutes on 48 cores */
+                        std::size_t per_block = 1<<23;
+
+                        std::atomic<bool> challenge_found = false;
+                        std::atomic<std::size_t> pow_value_offset;
+
+                        while( true ) {
+                            math::detail::block_execution(
+                                per_block, smp::count,
+                                [&transcript, &pow_seed, &challenge_found, &pow_value_offset]
+                                (std::size_t pow_start, std::size_t pow_finish) {
+                                    std::size_t i = pow_start;
+                                    while ( i < pow_finish ) {
+                                        if (challenge_found) {
+                                            break;
+                                        }
+                                        transcript_type tmp_transcript = transcript;
+                                        tmp_transcript(pow_seed + i);
+                                        integral_type pow_result = integral_type(tmp_transcript.template challenge<field_type>().data);
+                                        if ( ((pow_result & mask) == 0) && !challenge_found ) {
+                                            challenge_found = true;
+                                            pow_value_offset = i;
+                                            break;
+                                        }
+                                        ++i;
+                                    }
+                                }).get();
+
+                            if (challenge_found) {
+                                break;
+                            }
+                            pow_seed += per_block;
+                        }
+                        transcript(pow_seed + (std::size_t)pow_value_offset);
+                        transcript.template challenge<field_type>();
+                        return output_type(pow_seed + (std::size_t)pow_value_offset);
+                    }
+
+                    static inline bool verify(transcript_type &transcript, output_type const& proof_of_work) {
                         transcript(proof_of_work);
-                        integral_type result = integral_type(transcript.template challenge<FieldType>().data);
+                        integral_type result = integral_type(transcript.template challenge<field_type>().data);
                         return ((result & mask) == 0);
                     }
                 };
